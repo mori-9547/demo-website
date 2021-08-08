edadc638ec3034de4b8327a63528e9a25969a22c
yuya mori <crazyshironeko.0407@gmail.com>
2021-08-08 16:22:42 +0900

    first commit

---
+++ stylish-1/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../common/reset.css">
+    <link rel="stylesheet" href="/stylish-1/main.css">
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Lora&display=swap');
+    </style>
+    <title>Stylish Design No1</title>
+</head>
+<body>
+    <header class="header">
+        <div class="header-nav__item">
+            <a href="">Home</a>
+            <a href="">About</a>
+        </div>
+        <div class="header-nav__item header-nav__item--logo"></div>
+        <div class="header-nav__item">
+            <a href="">Menu</a>
+            <a href="">Contact</a>
+        </div>
+    </header>
+    <div class="home">
+        <div class="home-wrap">
+            <p class="home-title">
+                DEMO COFFEE
+            </p>
+        </div>
+    </div>
+</body>
+</html>
---
+++ vintage-1/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../common/reset.css">
+    <link rel="stylesheet" href="/vintage-1/main.css">
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Oswald:wght@400;500;600&display=swap');
+    </style>
+    <title>Vintage Design No1</title>
+</head>
+<body>
+    <header class="header">
+        <div class="logo"><img src="/vintage-1/images/handmade-logo.png" alt=""></div>
+        <ul class="header-nav">
+            <li class="header-nav__item">Home</li>
+            <li class="header-nav__item">About</li>
+            <li class="header-nav__item">News</li>
+            <li class="header-nav__item">Price</li>
+            <li class="header-nav__item">Contact</li>
+        </ul>
+    </header>
+    <div class="home">
+        <div class="home-wrap">
+            <div class="home-image"></div>
+            <p class="home-title">
+                Handmade<br>
+                Leather<br>
+                Wallet
+            </p>
+        </div>
+    </div>
+</body>
+</html>
