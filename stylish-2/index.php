966cf32c49b148343ecce8e372353b1cec352750
yuya mori <crazyshironeko.0407@gmail.com>
2021-08-09 14:19:41 +0900

    feat demo3

---
+++ stylish-2/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../common/reset.css">
+    <link rel="stylesheet" href="/stylish-2/main.css">
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&display=swap');
+    </style>
+    <title>Stylish Design No1</title>
+</head>
+<body>
+    <header class="header">
+        <div class="header-nav__item">
+            <a href="">Home</a>
+            <a href="">About</a>
+            <a href="">Menu</a>
+            <a href="">Contact</a>
+        </div>
+    </header>
+    <div class="wrap">
+        <figure class="home-img">
+            <img src="/stylish-2/images/woman.png">
+        </figure>
+        <h1 class="home-title">
+            Hair<br>
+            Cosmetics
+        </h1>
+        <div class="content"></div>
+    </div>
+</body>
+</html>
